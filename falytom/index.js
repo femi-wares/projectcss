3176259f3abbc14056e3e5ae0de663c74723e2f8
femi-wares <olorunfemiojo45@gmail.com>
2025-01-12 02:15:01 +0100

    Add files via upload
    hamburger menu presumed to not be working was ammended

--- falytom/index.js
+++ falytom/index.js
@@ -22,4 +22,32 @@ function intersectionObserver() {
 window.addEventListener("onload", intersectionObserver);
 
 
+/*  3d animation aspect of the file */
+
+//calling the 3d animation stuff
+const formContainer = document.querySelector('.form');
+const form = document.querySelector('form');
+const container = document.querySelector('.container');
+
+//calling the event listener for the form container
+
+container.addEventListener('mousemove' , (e) =>{
+   let xAxis = (window.innerWidth / 2 - e.pageX) /20;
+   let yAxis = (window.innerHeight / 2 - e.pageY)/20;
+    form.style.transform = `rotateY(${xAxis}deg) rotateY(${yAxis}deg)`;
+} );
+//animate in
+container.addEventListener('mouseenter' , (e) =>{
+    form.style.transition='none';
+});
+
+//animate out
+container.addEventListener('mouseleave' , (e) =>{
+    form.style.transition='all 0.6s ease';
+    form.style.transform =`rotateY(0deg) rotatex(0deg)`;
+});
+
+
+
+
 
